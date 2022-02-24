da53c1aaf9b5d9251ab42e63333c89c7c68dbe85
Lolboys <mateo.viel@epitech.eu>
2022-02-24 22:16:02 +0100

    inf loop need to find out why

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 import sys
 
 class Project:
-    def __init__(self, name, duration, award, best_before, requirements):
+    def __init__(self, name, duration, award, best_before, nb_roles, requirements):
         self.name = name
+        self.nb_roles = nb_roles
         self.duration = duration
         self.award = award
         self.best_before = best_before
@@ -29,16 +30,28 @@ def write_output(projects, mapping):
 
 def compute(projects, contributors):
     result = []
+    i = 0
 
-    for i in range(len(projects)):
+    while (i < len(projects)):
         contrib = []
+        original = projects[i]
+        y = 0
         for y in range(len(list(projects[i].requirements))):
             for x in range(len(contributors)):
                 if ([*projects[i].requirements][y] in list(contributors[x].abilities)):
-                    if (projects[i].requirements[[*projects[i].requirements][y]] > 0):
+                    if (projects[i].requirements[[*projects[i].requirements][y]] > 0 and projects[i].nb_roles > 0):
                         projects[i].requirements[[*projects[i].requirements][y]] -= contributors[x].abilities[[*projects[i].requirements][y]]
+                        projects[i].nb_roles -= 1
                         contrib.append(contributors[x].name)
+                        contributors[x].abilities[[*projects[i].requirements][y]] += 1
+            if (projects[i].requirements[[*projects[i].requirements][y]] > 0):
+                projects[i] = original
+                projects.append(projects.pop(i))
+                i -= 1
+                break
+        contrib = list(dict.fromkeys(contrib))
         result.append({projects[i].name: contrib})
+        i += 1
     return result
 
 def main(argv):
@@ -67,16 +80,16 @@ def main(argv):
         duration = int(buffer[count + 1].split()[1])
         reward = int(buffer[count + 1].split()[2])
         best_before = int(buffer[count + 1].split()[3])
-        nb_skills = int(buffer[count + 1].split()[4])
+        nb_roles = int(buffer[count + 1].split()[4])
         skills = {}
 
-        for y in range(0, nb_skills):
+        while ((count + 2 < len(buffer)) and len(buffer[count + 2].split()) == 2):
             skills[buffer[count + 2].split()[0]] = int(buffer[count + 2].split()[1])
             count += 1
-        projects.append(Project(name, duration, reward, best_before, skills))
+        projects.append(Project(name, duration, reward, best_before, nb_roles, skills))
         count += 1
 
-    result = compute(list(reversed(projects)), contributors)
+    result = compute(projects, contributors)
     write_output(nb_contributors, result)
 
 if __name__ == '__main__':
