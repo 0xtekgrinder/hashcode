8f2a32e531eeb43a6c373dd39d740e195251d99b
Lolboys <mateo.viel@epitech.eu>
2022-02-24 20:17:31 +0100

    feat: write output

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ class Contributor:
         self.name = name
         self.abilities = abilities
 
+def write_output(projects, mapping):
+    with open('result.txt', 'w') as f:
+        f.write(str(projects) + "\n")
+        for i in range(0, projects):
+            f.write([*mapping[i]][0] + "\n")
+            print(mapping[i][[*mapping[i]][0]])
+            for y in range(0, len(mapping[i][[*mapping[i]][0]])):
+                if (y != len(mapping[i][[*mapping[i]][0]]) - 1):
+                    f.write(mapping[i][[*mapping[i]][0]][y] + " ")
+                else:
+                    f.write(mapping[i][[*mapping[i]][0]][y] + "\n")
+        f.close()
+
 def main():
     with open('a_an_example.in.txt', 'r') as f:
         buffer = f.readlines()
+        f.close()
     nb_contributors =  int(buffer[0].split()[0])
     nb_projects = int(buffer[0].split()[1])
     contributors = []
@@ -44,6 +58,8 @@ def main():
             count += 1
         projects.append(Project(name, duration, reward, best_before, skills))
         count += 1
+    
+    write_output(3, [{"google": ["maria", "me"]}, {"google": ["maria", "me"]}, {"google": ["maria", "me"]}])
 
 if __name__ == '__main__':
     main()
